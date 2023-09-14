fda55bfd4f092c49f2267ad3aaaa0138903753ef
Samith <samithwijesinha@gmail.com>
2023-09-14 07:47:42 +0530

    add project files

---
+++ StarWarsWiki/StarWarsWikiApp.swift
@@ -0,0 +1,17 @@
+//
+//  StarWarsWikiApp.swift
+//  StarWarsWiki
+//
+//  Created by Samith on 2023-09-14.
+//
+
+import SwiftUI
+
+@main
+struct StarWarsWikiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
