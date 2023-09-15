d60e878b364f898c5daaee5662e33d66663612bb
Samith <samithwijesinha@gmail.com>
2023-09-15 10:28:09 +0530

    add detailed planet view

---
+++ StarWarsWiki/DetailPlanetView.swift
@@ -0,0 +1,47 @@
+//
+//  DetailPlanetView.swift
+//  StarWarsWiki
+//
+//  Created by Samith on 2023-09-15.
+//
+
+import SwiftUI
+
+struct DetailPlanetView: View {
+    let planets : Planets
+    var body: some View {
+        VStack(alignment: .leading){
+            Text(planets.name)
+                .font(.largeTitle)
+                .bold()
+            
+            Rectangle()
+                .frame(height: 2)
+                .frame(maxWidth: .infinity)
+                .foregroundColor(.gray)
+            
+            Group {
+                HStack {
+                    Text("Orbital Period :")
+                        .bold()
+                    Text(planets.orbital_period)
+                }
+                HStack {
+                    Text("Gravity :")
+                        .bold()
+                    Text(planets.gravity)
+                }
+            }
+            .font(.title2)
+ 
+            Spacer()
+        }
+        .padding()
+    }
+}
+
+struct DetailPlanetView_Previews: PreviewProvider {
+    static var previews: some View {
+        DetailPlanetView(planets: Planets(name: "Tatooine", orbital_period: "23", climate: "arid", gravity: "1 standard"))
+    }
+}
